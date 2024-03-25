6438f7b21223ba233fccf0af1ec5f3d7f2675355
Nick <nickphilomath@gmail.com>
2024-03-25 23:07:40 +0500

    connect vertices

--- drawMap.py
+++ drawMap.py
@@ -1,7 +1,6 @@
 import bpy
 import xml.etree.ElementTree as ET
 
-vertices_coordinates = []
 
 # load data from file
 xml_file_path = "data/map-small.osm"
@@ -12,18 +11,44 @@ bounds = root.findall('bounds')[0]
 minlat = float(bounds.get('minlat'))
 minlon = float(bounds.get('minlon'))
 
+
 # Iterate over each node element
+vertices_data = []
 for node in root.findall('node'):
     node_id = node.get('id')
     lat = float(node.get('lat')) - minlat
     lon = float(node.get('lon')) - minlon
     
-    vertices_coordinates.append((float(lat), float(lon), 0))
+    vertices_data.append({"id": int(node_id), "lat": float(lat), "lon": float(lon)})
+
+# create vertices coordinates for blender
+vertices_coordinates = []
+for vertex in vertices_data:
+    vertices_coordinates.append((vertex['lat'], vertex['lon'], 0))
+
+
+# Create a dictionary to map IDs to indices
+id_to_index = {obj["id"]: index for index, obj in enumerate(vertices_data)}
+
+
+# Iterate over each way element
+ways_data = []
+for way in root.findall('way'):
+    last_node = None
+    next_node = None
+    for nd in way.findall('nd'):
+        next_node = int(nd.get('ref'))
+        if last_node:
+            ways_data.append((id_to_index.get(last_node), id_to_index.get(next_node)))
+
+        last_node = next_node
+    #     print(nd.get('ref'))
+    # print()
 
 
 # load into blender
 mesh = bpy.data.meshes.new(name="VerticesMesh")
 obj = bpy.data.objects.new(name="VerticesObject", object_data=mesh)
 bpy.context.collection.objects.link(obj)
-mesh.from_pydata(vertices_coordinates, [], [])
+mesh.from_pydata(vertices_coordinates, ways_data, [])
 mesh.update()
